5274890a512e5dd45e1e3bee0267133c289bd353
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2021-01-31 20:50:00 +0100

    Create package API (#7)
    * [ADD] API and tests for functionality that needs to be implemented.
    
    * [GIT] Disable darglint and pydocstyle
    
    * [DEL] Remove dummy function from API, remove associated tests
    
    Co-authored-by: F. Dangel <fdangel@tuebingen.mpg.de>

--- test/test___init__.py
+++ test/test___init__.py
@@ -1,22 +1,37 @@
 """Tests for unfoldNd/__init__.py."""
 
-import unfoldNd
 import pytest
-import time
 
-NAMES = ["world", "github"]
-IDS = NAMES
+import unfoldNd
 
-
-@pytest.mark.parametrize("name", NAMES, ids=IDS)
-def test_hello(name):
-    """Test hello function."""
-    unfoldNd.hello(name)
+input = None
+kernel_size = None
+dilation = 1
+padding = 0
+stride = 1
 
 
-@pytest.mark.expensive
-@pytest.mark.parametrize("name", NAMES, ids=IDS)
-def test_hello_expensive(name):
-    """Expensive test of hello function. Will only be run on main and development."""
-    time.sleep(1)
-    unfoldNd.hello(name)
+def test_unfoldNd_not_implemented():
+    """``unfoldNd.unfoldNd`` is currently not implemented.
+
+    Raises:
+        NotImplementedError : always.
+    """
+    with pytest.raises(NotImplementedError):
+        unfoldNd.unfoldNd(
+            input, kernel_size, dilation=dilation, padding=padding, stride=stride
+        )
+
+
+def test_UnfoldNd_forward_not_implemented():
+    """``unfoldNd.UnfoldNd.forward`` is currently not implemented.
+
+    Raises:
+        NotImplementedError : always.
+    """
+    module = unfoldNd.UnfoldNd(
+        kernel_size, dilation=dilation, padding=padding, stride=stride
+    )
+
+    with pytest.raises(NotImplementedError):
+        module(input)
--- unfoldNd/__init__.py
+++ unfoldNd/__init__.py
@@ -1,10 +1,42 @@
 """unfoldNd library."""
 
+import torch.nn
 
-def hello(name):
-    """Say hello to a name.
 
-    Args:
-        name (str): Name to say hello to.
+class UnfoldNd(torch.nn.Module):
+    """Extracts sliding local blocks from a batched input tensor. Also known as im2col.
+
+    PyTorch module that accepts 3d, 4d, and 5d tensors. Acts like ``torch.nn.Unfold``
+    for a 4d input. Uses one-hot convolution under the hood.
+
+    See docs at https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html.
     """
-    print(f"Hello, {name}")
+
+    def __init__(self, kernel_size, dilation, padding=0, stride=1):
+        super().__init__()
+
+        self._kernel_size = kernel_size
+        self._dilation = dilation
+        self._padding = padding
+        self._stride = stride
+
+    def forward(self, input):
+        return unfoldNd(
+            input,
+            self._kernel_size,
+            dilation=self._dilation,
+            padding=self._padding,
+            stride=self._stride,
+        )
+
+
+def unfoldNd(input, kernel_size, dilation=1, padding=0, stride=1):
+    """Extracts sliding local blocks from an batched input tensor. Also known as im2col.
+
+    Pytorch functional that accepts 3d, 4d, and 5d tensors. Acts like
+    ``torch.nn.functional.unfold for a 4d input. Uses one-hot convolution under the
+    hood.
+
+    See docs at https://pytorch.org/docs/stable/nn.functional.html.
+    """
+    raise NotImplementedError
