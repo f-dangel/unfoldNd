6a9830280da72e2691e5dc7051cfeae54a1ca02e
F. Dangel <fdangel@tuebingen.mpg.de>
2021-02-07 22:50:02 +0100

    [TEST] Add test comparing Unfold1d with torch.nn.Unfold + dummy dim

--- test/settings.py
+++ test/settings.py
@@ -2,6 +2,58 @@
 
 from test.utils import make_id
 
+PROBLEMS_1D = [
+    {
+        "seed": 0,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 1,
+        },
+    },
+    {
+        "seed": 1,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 2,
+        },
+    },
+    {
+        "seed": 2,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 3,
+        },
+    },
+    {
+        "seed": 3,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 3,
+            "dilation": 2,
+        },
+    },
+    {
+        "seed": 4,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 3,
+            "dilation": 2,
+            "padding": 1,
+        },
+    },
+    {
+        "seed": 5,
+        "input_shape": (2, 3, 50),
+        "unfold_kwargs": {
+            "kernel_size": 3,
+            "dilation": 2,
+            "padding": 1,
+            "stride": 2,
+        },
+    },
+]
+PROBLEMS_1D_IDS = [make_id(problem) for problem in PROBLEMS_1D]
+
 PROBLEMS_2D = [
     {
         "seed": 0,
--- test/test___init__.py
+++ test/test___init__.py
@@ -1,6 +1,8 @@
 """Tests for unfoldNd/__init__.py."""
 
 from test.settings import (
+    PROBLEMS_1D,
+    PROBLEMS_1D_IDS,
     PROBLEMS_2D,
     PROBLEMS_2D_IDS,
     UNSUPPORTED_KERNEL_SIZE,
@@ -11,12 +13,13 @@ from test.settings import (
 
 import pytest
 import torch
+from torch.nn.modules.utils import _pair
 
 import unfoldNd
 
 
 @pytest.mark.parametrize("problem", PROBLEMS_2D, ids=PROBLEMS_2D_IDS)
-def test_Unfold2d(problem):
+def test_Unfold2d_vs_Unfold(problem):
     """Compare with ``torch.nn.Unfold`` for a 4d input."""
     seed = problem["seed"]
     input_shape = problem["input_shape"]
@@ -27,6 +30,37 @@ def test_Unfold2d(problem):
 
     result = unfoldNd.UnfoldNd(**unfold_kwargs)(inputs)
     result_torch = torch.nn.Unfold(**unfold_kwargs)(inputs)
+
+    assert torch.allclose(result, result_torch)
+
+
+@pytest.mark.parametrize("problem", PROBLEMS_1D, ids=PROBLEMS_1D_IDS)
+def test_Unfold1d_vs_dummy_dim_Unfold(problem):
+    """Compare with ``torch.nn.Unfold`` for a 3d input (adding a dummy dimension)."""
+    seed = problem["seed"]
+    input_shape = problem["input_shape"]
+    unfold_kwargs = problem["unfold_kwargs"]
+
+    torch.manual_seed(seed)
+    inputs = torch.rand(input_shape)
+
+    result = unfoldNd.UnfoldNd(**unfold_kwargs)(inputs)
+
+    def _add_dummy_dim(unfold_kwargs, inputs):
+        """Add dummy dimension to unfold hyperparameters and input."""
+        new_inputs = inputs.unsqueeze(-1)
+
+        new_kwargs = {}
+
+        for key, value in unfold_kwargs.items():
+            dummy = (0,) if key == "padding" else (1,)
+            new_value = _pair(value)[:-1] + dummy
+            new_kwargs[key] = new_value
+
+        return new_kwargs, new_inputs
+
+    unfold_kwargs_dummy_dim, inputs_dummy_dim = _add_dummy_dim(unfold_kwargs, inputs)
+    result_torch = torch.nn.Unfold(**unfold_kwargs_dummy_dim)(inputs_dummy_dim)
 
     assert torch.allclose(result, result_torch)
 
