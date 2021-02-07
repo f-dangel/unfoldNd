77cc6005271dbb90dd229b73f637ecf93cdc24c6
F. Dangel <fdangel@tuebingen.mpg.de>
2021-02-07 20:57:44 +0100

    [TEST] Add tests for unsupported inputs to helpers

--- test/test___init__.py
+++ test/test___init__.py
@@ -35,3 +35,32 @@ def test_UnfoldNd_forward_not_implemented():
 
     with pytest.raises(NotImplementedError):
         module(input)
+
+
+UNSUPPORTED_N = [4, -1]
+
+
+@pytest.mark.parametrize("N", UNSUPPORTED_N)
+def test__tuple_raise_dimension_error(N):
+    """Only N=1,2,3 are supported."""
+    dummy_kernel_size = None
+
+    with pytest.raises(ValueError):
+        unfoldNd._tuple(dummy_kernel_size, N)
+
+
+@pytest.mark.parametrize("N", UNSUPPORTED_N)
+def test__get_conv_raise_dimension_error(N):
+    """Only N=1,2,3 are supported."""
+    with pytest.raises(ValueError):
+        unfoldNd._get_conv(N)
+
+
+UNSUPPORTED_KERNEL_SIZE = [[1, 2], 1]
+
+
+@pytest.mark.parametrize("kernel_size", UNSUPPORTED_KERNEL_SIZE)
+def test__get_kenel_size_numel_raise_value_error(kernel_size):
+    """``kernel_size`` must be an ``N``-tuple."""
+    with pytest.raises(ValueError):
+        unfoldNd._get_kernel_size_numel(kernel_size)
