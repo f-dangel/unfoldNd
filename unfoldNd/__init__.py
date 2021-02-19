870c26d1edcd95845e0e726e7d5fb1a2f09e5899
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2021-02-19 15:19:05 +0100

    Revert "Avoid repeating kernel (#12)" (#13)
    This reverts commit 4c469186fe7e4d3ffd6829ac32a2500a7c3b535f.
    
    The benchmark results deteriorated both in terms of run time and memory consumption.

--- unfoldNd/__init__.py
+++ unfoldNd/__init__.py
@@ -45,7 +45,7 @@ def unfoldNd(input, kernel_size, dilation=1, padding=0, stride=1):
 
     See docs at https://pytorch.org/docs/stable/nn.functional.html.
     """
-    batch_size, in_channels, pixels = input.shape[0], input.shape[1], input.shape[2:]
+    batch_size, in_channels = input.shape[0], input.shape[1]
 
     # get convolution operation
     batch_size_and_in_channels_dims = 2
@@ -55,10 +55,7 @@ def unfoldNd(input, kernel_size, dilation=1, padding=0, stride=1):
     # prepare one-hot convolution kernel
     kernel_size = _tuple(kernel_size, N)
     kernel_size_numel = _get_kernel_size_numel(kernel_size)
-    weight = _make_weight(kernel_size, device=input.device)
-
-    # treat batch and channel dimension identically
-    input = input.reshape((batch_size * in_channels, 1, *pixels))
+    weight = _make_weight(in_channels, kernel_size, device=input.device)
 
     unfold = conv(
         input,
@@ -67,6 +64,7 @@ def unfoldNd(input, kernel_size, dilation=1, padding=0, stride=1):
         stride=stride,
         padding=padding,
         dilation=dilation,
+        groups=in_channels,
     )
 
     return unfold.reshape(batch_size, in_channels * kernel_size_numel, -1)
@@ -101,18 +99,20 @@ def _raise_dimension_error(N):
     raise ValueError(f"Only 1,2,3-dimensional unfold is supported. Got N={N}.")
 
 
-def _make_weight(kernel_size, device):
+def _make_weight(in_channels, kernel_size, device):
     """Create one-hot convolution kernel. ``kernel_size`` must be an ``N``-tuple.
 
     Details:
-        Let ``T`` denote the one-hot weight, then ``T[i, 0, j] = δᵢⱼ`` (``j`` is a
-        group index of the ``Kᵢ``).
+        Let ``T`` denote the one-hot weight, then
+        ``T[c * i, 0, j] = δᵢⱼ ∀ c = 1, ... C_in``
+        (``j`` is a group index of the ``Kᵢ``).
 
-        This can be done by building diagonals ``D[i, j] = δᵢⱼ`` and reshaping
-        them into ``[∏ᵢ Kᵢ, 1, K]``.
+        This can be done by building diagonals ``D[i, j] = δᵢⱼ``, reshaping
+        them into ``[∏ᵢ Kᵢ, 1, K]``, and repeat them ``C_in`` times along the
+        leading dimension.
 
     Returns:
-        torch.Tensor : A tensor of shape ``[∏ᵢ Kᵢ, 1, K]`` where
+        torch.Tensor : A tensor of shape ``[ C_in * ∏ᵢ Kᵢ, 1, K]`` where
             ``K = (K₁, K₂, ..., Kₙ)`` is the kernel size. Filter groups are
             one-hot such that they effectively extract one element of the patch
             the kernel currently overlaps with.
@@ -120,9 +120,12 @@ def _make_weight(kernel_size, device):
 
     """
     kernel_size_numel = _get_kernel_size_numel(kernel_size)
+    repeat = [in_channels, 1] + [1 for _ in kernel_size]
 
-    return torch.eye(kernel_size_numel, device=device).reshape(
-        (kernel_size_numel, 1, *kernel_size)
+    return (
+        torch.eye(kernel_size_numel, device=device)
+        .reshape((kernel_size_numel, 1, *kernel_size))
+        .repeat(*repeat)
     )
 
 
