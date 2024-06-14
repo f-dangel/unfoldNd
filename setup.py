64ee78e479202cb764c3e70d27994294aeb77546
Anton <12076257+xaf-cv@users.noreply.github.com>
2024-06-14 16:32:05 +0200

    [REF] Port package version discovery from pkg_resources [deprecated] to importlib+distutils (#37)
    * * port from pkg_resources [deprecated] to importlib+distutils;
    
    * * bump minimum version to 3.8;
    
    * * black file formatting;
    
    * * fix setup.cfg for flake8 v6 parsing;
    
    ---------
    
    Co-authored-by: Anton <anton.frolov@uni-weimar.de>

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@ Use setup.cfg to configure the project.
 """
 
 import sys
+from distutils.version import LooseVersion
+from importlib.metadata import PackageNotFoundError, version
 
-from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require("setuptools>=38.3")
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
+    setuptools_version = LooseVersion(version("setuptools"))
+    required_version = LooseVersion("38.3")
+    if setuptools_version < required_version:
+        print("Error: version of setuptools is too old (<38.3)!")
+        sys.exit(1)
+except PackageNotFoundError:
+    print("setuptools not found")
     sys.exit(1)
-
 
 if __name__ == "__main__":
     setup(use_scm_version=True)
--- unfoldNd/__init__.py
+++ unfoldNd/__init__.py
@@ -1,6 +1,5 @@
 """unfoldNd library."""
 
-
 from unfoldNd.fold import FoldNd, foldNd
 from unfoldNd.unfold import UnfoldNd, unfoldNd
 from unfoldNd.unfold_transpose import UnfoldTransposeNd, unfold_transposeNd
